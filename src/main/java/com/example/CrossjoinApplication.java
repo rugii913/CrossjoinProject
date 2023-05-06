4f4175684eb6c8bab03fab2ae3e147113f880cd9
rugii913 <rugii913@gmail.com>
2023-05-06 15:59:33 +0900

    개인 프로젝트 시작

---
+++ src/main/java/com/example/CrossjoinApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossjoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossjoinApplication.class, args);
+	}
+
+}
