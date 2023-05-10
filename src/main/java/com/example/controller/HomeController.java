920a99e8c47478d063b687472aad971f4791f988
rugii913 <rugii913@gmail.com>
2023-05-11 01:25:25 +0900

    JSP, Thymeleaf 동시에 사용 가능하도록 application.yml 수정
    ㅁpom.xml에서 thymeleaf dependency 주석 해제
    ㅁapplication.yml에 thymeleaf prefix, suffix 및 view-names 설정 추가
     ㅇ여기서 view-names 설정이 핵심인 듯함
    
    *****
    (먼저 참고한 링크)
    https://hotsse.github.io/articles/2022-02/Spring-Thymeleaf-%EC%99%80-JSP-%EB%8F%99%EC%8B%9C%EC%97%90-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
    -> 뭔가 돌아가는 것처럼 보였으나 서버 뜰 때부터 WARN 로그 발생함
    ...$DefaultTemplateResolverConfiguration...어쩌구 Cannot find template location: resouces/ (please add some templates, check your Thymeleaf configuration, or set spring.thymeleaf.check-template-location=false)
    
    application.yml prefix 부분에 문제 있는 듯하여 Cannot find template location로 검색
    
    (그 다음 참고 링크)
    https://lifere.tistory.com/entry/WARN-Cannot-find-template-location-classpathtemplates-please-add-some-templates-check-your-Thymeleaf-configuration-or-set-springthymeleafcheck-template-locationfalse
    -> 몇 번 더 삽질하다가 classpath: 적어주면서 경로 바꿔보면서 지정해주니 돌아가긴 함
    classpath: 없이 prefix: /로만 적어도 에러 발생함
    
    잘 이해는 안 되니 나중에 다시 찾아볼 것

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping({"/home/main", "/"})
+	@GetMapping({"/home/main", "/", ""})
 	public String showMain() {
 		
 		return "/usr/home/main";
+	}
+	
+	@GetMapping("/th")
+	public String th() {
+		return "templates/temp";
 	}
 }
