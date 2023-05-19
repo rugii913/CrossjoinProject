893c2df9e403124148547e4c096b9f236c9a44d0
rugii913 <rugii913@gmail.com>
2023-05-19 14:31:55 +0900

    ArticleController 및 ArtiControllerImpl 주석 달기

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -8,17 +8,50 @@ import com.example.dto.ArticleDto;
 
 public interface ArticleController {
 	
+	/**
+	 * 일반 게시판 글쓰기 뷰 jsp로 연결하는 메서드
+	 * 반환값: String(jsp 경로)
+	 */
 	public String viewWriteArticleWindow();
 	
+	/**
+	 * 일반 게시판 글 데이터 저장 메서드
+	 * 매개변수: ArticleDto 타입 게시물 데이터, Model 타입 스프링프레임워크 모델
+	 * 반환값: String(게시물 뷰 jsp 경로: showArticle 메서드 호출 -> 저장된 글 id와 모델을 매개변수로 넘김)
+	 */
 	public String writeArticle(ArticleDto articleData, Model model);
 
+	/**
+	 * 글 목록 가져오는 메서드
+	 * 매개변수: Integer 게시판 아이디(디폴트 값 0), Integer 게시판 페이지 값
+	 * 반환값: List<ArticleDto>
+	 */
 	public List<ArticleDto> showArticleList(Integer boardId, Integer page);
 	
+	/**
+	 * 특정 게시물을 보여주는 메서드
+	 * 매개변수: int 게시물 아이디, Integer 게시판 페이지 값, Model 스프링프레임워크 모델
+	 */
 	public String showArticle(int id, Model model);
 	
+	/**
+	 * 일반 게시판 게시물을 수정하는 뷰 jsp로 연결하는 메서드
+	 * 매개변수: int 게시물 아이디, Model 스프링프레임워크 모델
+	 * 반환값: String(jsp 경로)
+	 */
 	public String viewModifyArticleWindow(int id, Model model);
 	
+	/**
+	 * 일반 게시판 게시물을 내용을 수정하도록 저장하는 메서드
+	 * 매개변수: int 게시물 아이디, ArticleDto 타입 수정할 게시물 데이터
+	 * 반환값: String(리다이렉트 url)
+	 */
 	public String modifyArticle(Integer id, ArticleDto articleData);
 	
+	/**
+	 * 일반 게시판 게시물을 삭제하는 메서드
+	 * 매개변수: int 게시물 아이디
+	 * 반환값: 없음
+	 */
 	public void deleteArticle(int id);
 }
--- src/main/java/com/example/controller/ArticleControllerImpl.java
+++ src/main/java/com/example/controller/ArticleControllerImpl.java
@@ -22,6 +22,11 @@ public class ArticleControllerImpl implements ArticleController{
 		this.articleService = articleService;
 	}
 	
+	/**
+	 * 일반 게시판 글쓰기 뷰 jsp로 연결하는 메서드
+	 * 매개변수: 없음
+	 * 반환값: String(jsp 경로)
+	 */
 	@GetMapping("/write")
 	@Override
 	public String viewWriteArticleWindow() {
@@ -29,6 +34,11 @@ public class ArticleControllerImpl implements ArticleController{
 		return "/usr/article/general/write";
 	}
 	
+	/**
+	 * 일반 게시판 글 데이터 저장 메서드
+	 * 매개변수: ArticleDto 타입 게시물 데이터, Model 스프링프레임워크 모델
+	 * 반환값: String(게시물 뷰 jsp 경로: showArticle 메서드 호출 -> 저장된 글 id와 모델을 매개변수로 넘김)
+	 */
 	@PostMapping("/write")
 	@Override
 	public String writeArticle(ArticleDto articleData, Model model) {
@@ -37,6 +47,13 @@ public class ArticleControllerImpl implements ArticleController{
 		return showArticle(id, model);
 	}
 	
+	/**
+	 * 일반 게시판 글 목록 뷰 jsp로 연결하는 메서드
+	 * 메서드 내부에서 글 목록 가져오는 메서드 호출해서 List를 Model에 담아서 뷰로 넘김
+	 * *** 인터페이스에서 override한 메서드 아님 ***
+	 * 매개변수: Model 스프링프레임워크 모델, Integer 게시판 아이디(디폴트 값 0), Integer 게시판 페이지 값 
+	 * 반환값: String(jsp 경로)
+	 */
 	@GetMapping // http://localhost:8081/article?boardId=0
 	public String viewArticleList(Model model, @RequestParam(defaultValue = "0") Integer boardId, @RequestParam(defaultValue = "1") Integer page) {
 		
@@ -46,6 +63,11 @@ public class ArticleControllerImpl implements ArticleController{
 		return "/usr/article/general/list";
 	}
 	
+	/**
+	 * 일반 게시판 글 목록 가져오는 메서드
+	 * 매개변수: Integer 게시판 아이디(디폴트 값 0), Integer 게시판 페이지 값
+	 * 반환값: List<ArticleDto> 게시물 목록
+	 */
 	//url 매핑 없으나, ArticleController 인터페이스 유지하기 위해 남겨놓는 부분 시작 - viewArticleList 관련
 //	@GetMapping("/list")
 //	@ResponseBody
@@ -60,6 +82,11 @@ public class ArticleControllerImpl implements ArticleController{
 	}
 	//url 매핑 없으나, ArticleController 인터페이스 유지하기 위해 남겨놓는 부분 끝 - viewArticleList 관련
 
+	/**
+	 * 일반 게시판 게시물을 보여주는 메서드
+	 * 매개변수: int 게시물 아이디, Integer 게시판 페이지 값, Model 스프링프레임워크 모델
+	 * 반환값: String(jsp 경로)
+	 */
 	@GetMapping("/{articleId}")
 	@Override
 	public String showArticle(@PathVariable("articleId") int id, Model model) {
@@ -70,6 +97,11 @@ public class ArticleControllerImpl implements ArticleController{
 		return "/usr/article/general/detail";
 	}
 	
+	/**
+	 * 일반 게시판 게시물을 수정하는 뷰 jsp로 연결하는 메서드
+	 * 매개변수: int 게시물 아이디, Model 스프링프레임워크 모델
+	 * 반환값: String(jsp 경로)
+	 */
 	@GetMapping("/{articleId}/modify")
 	@Override
 	public String viewModifyArticleWindow(@PathVariable("articleId") int id, Model model) {
@@ -80,6 +112,11 @@ public class ArticleControllerImpl implements ArticleController{
 		return "/usr/article/general/modify";
 	}
 	
+	/**
+	 * 일반 게시판 게시물을 내용을 수정하도록 저장하는 메서드
+	 * 매개변수: int 게시물 아이디, ArticleDto 타입 수정할 게시물 데이터
+	 * 반환값: String(리다이렉트 url)
+	 */
 	@PostMapping("/{articleId}/modify")
 	@Override
 	public String modifyArticle(@PathVariable("articleId") Integer id, ArticleDto articleData) {
@@ -95,6 +132,11 @@ public class ArticleControllerImpl implements ArticleController{
 		return "redirect:/article/{articleId}";
 	}
 	
+	/**
+	 * 일반 게시판 게시물을 삭제하는 메서드
+	 * 매개변수: int 게시물 아이디
+	 * 반환값: 없음
+	 */
 	@PostMapping("/{articleId}/delete")
 	@Override
 	public void deleteArticle(@PathVariable("articleId") int id) {
