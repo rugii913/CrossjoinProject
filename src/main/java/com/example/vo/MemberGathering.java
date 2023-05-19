54e128453e6356c546849ca7f1b4c74dfc6fd071
rugii913 <rugii913@gmail.com>
2023-05-19 16:22:19 +0900

    MemberGathering vo 추가(M:N 관계)

---
+++ src/main/java/com/example/vo/MemberGathering.java
@@ -0,0 +1,14 @@
+package com.example.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberGathering {
+	
+	private int memberId;
+	private int atheringId;
+	private boolean host;
+
+}
